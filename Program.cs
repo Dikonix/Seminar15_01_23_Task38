c88bd47fdeca990febd0a6d9999df03d3f18c3e2
Diana Brik <di.brik@yandex.kz>
2023-01-18 10:40:34 +0600

    Seminar 15.01.23 Task 38

---
+++ Program.cs
@@ -0,0 +1,53 @@
+﻿int[] CreateRandomArray(int sizeArray, int start, int end)
+{
+    int[] randomArray = new int[sizeArray];
+    for (int i = 0; i < sizeArray; i++)
+    {
+        randomArray[i] = new Random().Next(start, end);
+    }
+    return randomArray;
+}
+
+void ShowArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.WriteLine();
+}
+
+int MaxNumber(int[] array)
+{
+    int max = array[0];
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > max)
+        {
+            max = array[i];
+        }
+    }
+    return max;
+}
+
+int MinNumber(int[] array)
+{
+    int min = array[0];
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] < min)
+        {
+            min = array[i];
+        }
+    }
+    return min;
+}
+
+int[] array = CreateRandomArray(6, 0, 100);
+ShowArray(array);
+
+int max = MaxNumber(array);
+int min = MinNumber(array);
+int difference = max - min;
+
+Console.WriteLine($"Разница между {max} и {min} равна {difference}");
